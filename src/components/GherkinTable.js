08f2d81775d164f9b39a739dc79f3cd05d57f99a
vienkarsialeksis <ipb21.a.osins@vtdt.edu.lv>
2023-10-31 11:37:13 +0200

    gherkin

---
+++ src/components/GherkinTable.js
@@ -0,0 +1,56 @@
+function GherkinTable() {
+    return (
+        <table>
+      <thead>
+        <tr>
+          <th>English Keyword</th>
+          <th>Latvian equivalent(s)</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>feature</td>
+          <td>Funkcionalitāte</td>
+        </tr>
+        <tr>
+          <td>background</td>
+          <td>Konteksts</td>
+        </tr>
+        <tr>
+          <td>scenario</td>
+          <td>Scenārijs</td>
+        </tr>
+        <tr>
+          <td>scenarioOutline</td>
+          <td>Scenārijs pēc parauga</td>
+        </tr>
+        <tr>
+          <td>examples</td>
+          <td>Piemēri</td>
+        </tr>
+        <tr>
+          <td>given</td>
+          <td>Kad</td>
+        </tr>
+        <tr>
+          <td>when</td>
+          <td>Ja</td>
+        </tr>
+        <tr>
+          <td>then</td>
+          <td>Tad</td>
+        </tr>
+        <tr>
+          <td>and</td>
+          <td>Un</td>
+        </tr>
+        <tr>
+          <td>or</td>
+          <td>Bet</td>
+        </tr>
+      </tbody>
+    </table>
+    );
+}
+
+export default GherkinTable;
